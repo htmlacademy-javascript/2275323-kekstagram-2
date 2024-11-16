7808095c9057c450c3143b7fc941aa03d17e1118
Женя Балабанов <biomannumberone@gmail.com>
2024-11-16 15:24:26 +0300

    Создаст файл consts, в котором содержатся константы, и импортирует их в файл main

---
+++ js/consts.js
@@ -0,0 +1,45 @@
+const COMMENTS_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Tim',
+  'Tom',
+  'John',
+  'Ann',
+  'Persik',
+  'Keks',
+  'Pashtet',
+  'Myrzik',
+  'Pinokio'
+];
+
+const AVATAR_COUNT = 6;
+const PHOTOS_COUNT = 25;
+
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
+const MIN_MESSAGES = 1;
+const MAX_MESSAGES = 2;
+
+export {
+  COMMENTS_MESSAGES,
+  NAMES,
+  AVATAR_COUNT,
+  PHOTOS_COUNT,
+  MIN_COMMENTS,
+  MAX_COMMENTS,
+  MIN_LIKES,
+  MAX_LIKES,
+  MIN_MESSAGES,
+  MAX_MESSAGES
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+import {
+  COMMENTS_MESSAGES,
+  NAMES,
+  AVATAR_COUNT,
+  PHOTOS_COUNT,
+  MIN_COMMENTS,
+  MAX_COMMENTS,
+  MIN_LIKES,
+  MAX_LIKES,
+  MIN_MESSAGES,
+  MAX_MESSAGES
+} from './consts.js';
+
 function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -9,13 +22,13 @@ function getRandomElement(array) {
 function createComment(id, messages, names) {
   const arrMessages = [];
 
-  for (let i = 0; i < getRandomValue(1, 2); i += 1) {
+  for (let i = 0; i < getRandomValue(MIN_MESSAGES, MAX_MESSAGES); i++) {
     arrMessages.push(getRandomElement(messages));
   }
 
   return {
     id: id,
-    avatar: `img/avatar-${ getRandomValue(1, 6) }.svg`,
+    avatar: `img/avatar-${ getRandomValue(1, AVATAR_COUNT) }.svg`,
     message: arrMessages.join(' '),
     name: getRandomElement(names)
   };
@@ -24,11 +37,11 @@ function createComment(id, messages, names) {
 function createPhotos(messages, names) {
   const photo = [];
 
-  for (let i = 1; i <= 25; i += 1) {
+  for (let i = 1; i <= PHOTOS_COUNT; i++) {
     const comments = [];
-    const commentsCount = getRandomValue(0, 30);
+    const commentsCount = getRandomValue(MIN_COMMENTS, MAX_COMMENTS);
 
-    for(let j = 1; j <= commentsCount; j += 1) {
+    for(let j = 1; j <= commentsCount; j++) {
       comments.push(createComment(j, messages, names));
     }
 
@@ -36,33 +49,12 @@ function createPhotos(messages, names) {
       id: i,
       url: `photos/${ i }.jpg`,
       description: `Фотография ${ i }`,
-      likes: getRandomValue(15, 200),
+      likes: getRandomValue(MIN_LIKES, MAX_LIKES),
       comments: comments
     });
   }
 
   return photo;
 }
-
-const COMMENTS_MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAMES = [
-  'Tim',
-  'Tom',
-  'John',
-  'Ann',
-  'Persik',
-  'Keks',
-  'Pashtet',
-  'Myrzik',
-  'Pinokio'
-];
 
 createPhotos(COMMENTS_MESSAGES, NAMES);
